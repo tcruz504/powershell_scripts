d2fb51bd72b30f0d1770216580846d67cafd3c16
Tomas Cruz <lawilsada@gmail.com>
2019-09-09 12:03:29 -0500

    Update CheckForServersForPatches.ps1

--- CheckForServersForPatches.ps1
+++ CheckForServersForPatches.ps1
@@ -1,4 +1,4 @@
-﻿get-adcomputer -filter {Enabled -eq $true} -server corp.trivin.com -Properties CanonicalName | foreach-object{
+﻿get-adcomputer -filter {Enabled -eq $true} -server ****** -Properties CanonicalName | foreach-object{
     
     #Output name of computer we're checking
     $status = "Checking "+$_.Name; Write-Host $status;
@@ -12,10 +12,10 @@
     
     #Output the server name, CN, and whether the patch is installed to a summary file
     #$_ | Select-Object @{Name="Server";Expression={$_.name}}, @{Name="CN";Expression={$_.CanonicalName}}, @{Name="MS017-010 Installed?";Expression={$installed}} | Export-Csv -Path MS017-010_Summary.csv -Append -NoTypeInformation;
-    $_ | Select-Object Name, CanonicalName, @{Name="MS017-010 Installed?";Expression={$installed}} | Export-Csv -Path C:\Users\tomas.cruz.DTINC\Desktop\Patches\MS017-010_Summary.csv -Append -NoTypeInformation;
+    $_ | Select-Object Name, CanonicalName, @{Name="MS017-010 Installed?";Expression={$installed}} | Export-Csv -Path C:\Users\"Destination"\Desktop\Patches\MS017-010_Summary.csv -Append -NoTypeInformation;
     
     #Output specific installed patch information to another file
-    $patches | Export-Csv -Path C:\Users\tomas.cruz.DTINC\Desktop\Patches\MS017-010.csv -Append -NoTypeInformation
+    $patches | Export-Csv -Path C:\Users\"destination"\Desktop\Patches\MS017-010.csv -Append -NoTypeInformation
 
 } 
 

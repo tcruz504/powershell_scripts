200a7c1a01b916e51dfc0e39452b8f077f89aadf
Tomas Cruz <lawilsada@gmail.com>
2019-09-09 12:16:08 -0500

    Update GroupMemberAdd.ps1

--- GroupMemberAdd.ps1
+++ GroupMemberAdd.ps1
@@ -1,2 +1,2 @@
-﻿foreach ($group in (Get-ADUser -Identity "Michael.McCormick" -Properties MemberOf).MemberOf) 
-{   Add-ADGroupMember -Identity $group -Members "Christine.Loveless"; }
+﻿foreach ($group in (Get-ADUser -Identity "sourceUser" -Properties MemberOf).MemberOf) 
+{   Add-ADGroupMember -Identity $group -Members "destinationUserr"; }

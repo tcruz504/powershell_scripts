491d33efd70cfbe694ff34cb8ef7b21b611a7cbd
Tomas Cruz <lawilsada@gmail.com>
2019-09-09 12:07:50 -0500

    Update CheckdailyStatusofSpiceworks.ps1

--- CheckdailyStatusofSpiceworks.ps1
+++ CheckdailyStatusofSpiceworks.ps1
@@ -1,5 +1,5 @@
 ﻿#Run this everyday to check the status of all Helpdesk Systems
 
-$Helpdesk = "Met-helpdesk2", "Met-helpdesk3", "Met-helpdesk4"
+$Helpdesk = ""
 $Service = "Spiceworks"
 Get-Service -Name $Service -ComputerName $Helpdesk | Select Name, Displayname, Status, Machinename | format-list | Get-date | select Day, Month, Year| Out-File -FilePath c:\status.log -Append

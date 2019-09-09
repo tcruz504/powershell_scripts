67887d3367d01c198b414fee897030668589ecc0
Tomas Cruz <lawilsada@gmail.com>
2019-09-09 12:29:23 -0500

    Delete RestartALLrtslaPCs.ps1

--- RestartALLrtslaPCs.ps1
+++
@@ -1,7 +0,0 @@
-﻿$City = Read-Host 'City'
-$ou = "OU=Workstations,OU=$City,DC=dt,DC=inc"
-
-$Workstations = Get-ADComputer -Filter '*' -SearchBase $ou
-
-$Workstations | Foreach { $_.DNSHostName } | Out-File c:\RTSLAworkstationList.txt 
-

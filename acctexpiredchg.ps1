7f3dea5dd6952e35fe8d959686091ab5f1e786db
Tomas Cruz <lawilsada@gmail.com>
2019-09-09 11:55:05 -0500

    Delete acctexpiredchg.ps1

--- acctexpiredchg.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-Get-Content C:\Srvs.txt | % 
-
-{
-Get-WmiObject -Class Win32_UserAccount -ComputerName $_ -Filter "name = 'administrator'"  | Set-WmiInstance -Argument @{PasswordExpires = 1}
-                
- }
-

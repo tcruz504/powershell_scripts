905a0db631d150a556d50e4a254588c172fc4fec
Tomas Cruz <lawilsada@gmail.com>
2019-09-09 12:17:22 -0500

    Update InstallationScript.ps1

--- InstallationScript.ps1
+++ InstallationScript.ps1
@@ -1,5 +1,5 @@
-﻿$servers = "GRO-FORM01W-C", "GRO-FORM02-W"
-$source = "\\GRO-FLS01W-B\Software\PatchLink\PatchLink Agent v8_2_0_10 - 2015\8.2.0.10 (HQ_DEV_QA_DR)\lmsetup_8.2.0.10x64.exe"
+﻿$servers = "", ""
+$source = "\\sourcelocation\Software\PatchLink\PatchLink Agent v8_2_0_10 - 2015\8.2.0.10 ()\lmsetup_8.2.0.10x64.exe"
 #$arguments = "/Install /S /SERVERIPADDRESS=192.168.10.223 /serial=A3038A2E-99122227"
 
 
